3900077f049c969fd3078e44c088d07767f93bdf
Nishant <ni6hant@gmail.com>
2023-01-03 14:25:14 +0530

    final assignemnt in first course
    smallest and largest

--- computePay.py
+++ computePay.py
@@ -1,6 +1,33 @@
-def addtwo(a, b):
-    added = a + b
-    return a
+# Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
+# Do not worry about error checking the user input unless you want to - you can assume the user types numbers properly.
+# Do not name your variable sum or use the sum() function.
 
-x = addtwo(2, 7)
-print(x)
+def computepay(h, r):
+# Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for all hours worked above 40 hours.
+# Put the logic to do the computation of pay in a function called computepay() and use the function to do the computation. The function should return a value. 
+    if hours<=40:
+        return (hours*ratesPerHour)
+    else:
+        return ((40*ratesPerHour)+(hours-40)*(1.5*ratesPerHour))
+
+# 4.6 Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
+hrs = input("Enter Hours: ")
+
+# You should use input to read a string and float() to convert the string to a number.
+try:
+    hours = float(hrs)
+except:
+    print("Please enter hours in Numerals only without commas");
+    quit()
+
+rps = input("Input Rates per hour: ")
+
+try:
+    ratesPerHour = float(rps)
+except:
+    print("Please enter Rates Per Hour in Numerals only without commas");
+    quit()
+
+print("Pay",computepay(hours,ratesPerHour));
+
+
---
+++ loopExercise.py
@@ -0,0 +1,32 @@
+# 5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'.
+# Once 'done' is entered, print out the largest and smallest of the numbers.
+# If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number.
+# Enter 7, 2, bob, 10, and 4 and match the output below.
+i=0
+
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+       
+    try:
+        number = int(num)
+    except:
+        print("The value",num,"is ignored.");
+        continue
+    
+    if i==0 :
+        largest = number
+        smallest = number
+    i=i+1
+    
+    print(number)
+
+    if number>largest:
+        largest = number
+    
+    if number<smallest:
+        smallest = number
+
+print("Maximum", largest)
+print("Minimum", smallest)
