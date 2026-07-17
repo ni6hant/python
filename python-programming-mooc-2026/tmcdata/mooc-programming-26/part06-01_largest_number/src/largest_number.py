722263f8131c7727ab984540525b5f31e723f3a3
ni6hant <ni6hant@gmail.com>
2026-07-17 16:37:14 +0530

    asdf

--- python-programming-mooc-2026/tmcdata/mooc-programming-26/part06-01_largest_number/src/largest_number.py
+++ python-programming-mooc-2026/tmcdata/mooc-programming-26/part06-01_largest_number/src/largest_number.py
@@ -1,2 +1,20 @@
 # write your solution here
+def largest():
+    with open("numbers.txt") as new_file:
 
+        flag = True
+
+        for line in new_file:
+            line = int(line.replace("\n",""))
+
+            #store the first value then skip all else
+            if flag:
+                largest_number = line
+            flag = False
+
+            if largest_number<line:
+                largest_number = line
+    return largest_number
+
+if __name__ == "__main__":
+    largest()
