30381c5d249a0b86cc709397370e9bde36618651
ni6hant <ni6hant@gmail.com>
2026-07-18 21:15:45 +0530

    0

--- python-programming-mooc-2026/tmcdata/mooc-programming-26/part05-10_sudoku_print_and_add/src/sudoku_print_and_add.py
+++ python-programming-mooc-2026/tmcdata/mooc-programming-26/part05-10_sudoku_print_and_add/src/sudoku_print_and_add.py
@@ -1,1 +1,53 @@
 # Write your solution here
+def print_row(row:list):
+    count = 0
+    for item in row:
+        #if 0 replace with -
+        if item==0:
+            print("_",end="")
+        else:
+            print(item,end="")
+
+        #add two space after 3,6 and for rest add one space after except from the last one
+        count+=1
+        if count==9:
+            print("")
+        elif count%3==0:
+            print("  ",end="")
+        else: #count%3!=0
+            print(" ",end="")
+
+def print_sudoku(sudoku:list):
+    count=0
+    #loop through each row
+    for row in sudoku:
+        print_row(row) #print each row
+        count+=1
+        if count%3==0 and count%9!=0: #after 3,6 row add a blank line
+            print()
+    return
+
+def add_number(sudoku:list, row_no:int, column_no:int, number:int):
+    sudoku[row_no][column_no]=number
+    return sudoku
+
+if __name__ == "__main__":
+    sudoku  = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    print_sudoku(sudoku)
+    add_number(sudoku, 0, 0, 2)
+    add_number(sudoku, 1, 2, 7)
+    add_number(sudoku, 5, 7, 3)
+    print()
+    print("Three numbers added:")
+    print()
+    print_sudoku(sudoku)
--- python-programming-mooc-2026/tmcdata/mooc-programming-26/part05-11_sudoku_add_to_copy/src/sudoku_add_to_copy.py
+++ python-programming-mooc-2026/tmcdata/mooc-programming-26/part05-11_sudoku_add_to_copy/src/sudoku_add_to_copy.py
@@ -1,1 +1,58 @@
 # Write your solution here
+def print_row(row:list):
+    count = 0
+    for item in row:
+        #if 0 replace with -
+        if item==0:
+            print("_",end="")
+        else:
+            print(item,end="")
+
+        #add two space after 3,6 and for rest add one space after except from the last one
+        count+=1
+        if count==9:
+            print("")
+        elif count%3==0:
+            print("  ",end="")
+        else: #count%3!=0
+            print(" ",end="")
+
+def print_sudoku(sudoku:list):
+    count=0
+    #loop through each row
+    for row in sudoku:
+        print_row(row) #print each row
+        count+=1
+        if count%3==0 and count%9!=0: #after 3,6 row add a blank line
+            print()
+    return
+
+
+def copy_and_add(sudoku:list, row_no:int, column_no:int, number:int):
+    copy_sudoku=[[]]*9
+    count=0
+    for row in sudoku:
+        copy_sudoku[count][:]=row
+        count+=1
+    copy_sudoku[row_no][column_no]=number
+    return copy_sudoku
+
+if __name__=="__main__":
+    sudoku  = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    grid_copy = copy_and_add(sudoku, 0, 0, 2)
+    print("Original:")
+    print_sudoku(sudoku)
+    print()
+    print("Copy:")
+    print_sudoku(grid_copy)
